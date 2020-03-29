82e1e82069cd88a75260f4327bc7c6846bdee695
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-03-29 19:45:48 +0530

    added factorial function to loops.c

--- src/loops.c
+++ src/loops.c
@@ -1,25 +1,43 @@
 #include <stdio.h>
 
-int factorial(int);
+long long int factorial(long long int);
+void fibonacci(int);
 
-int factorial(int num)
+long long int factorial(long long int num)
 {
-  int result = 1;
+  long long int result = 1;
   if(num <= 0){
     return result;
   }
- for(int term = num ; term > 1 ; term--){
+ for(long long int term = num ; term > 1 ; term--){
    result = result * term;
  }
   return result;
 }
 
+void fibonacci(int range)
+{
+  int count = range;
+  int num1 = 0;
+  int num2 = 1;
+  int sum_of_previous_terms;
+  for(int index = 1 ; index <= count ; index++){
+    printf("%d\n",num1);
+    sum_of_previous_terms = num1 + num2;
+    num1 = num2;
+    num2 = sum_of_previous_terms;
+  }
+}
+
 int main(void)
 {
-  int num;
+  long long int num;
 
   printf("Write number here for factorial :");
-  scanf("%d",&num);
+  scanf("%lld",&num);
+  printf("Factorial of given number is %lld\n",factorial(num));
 
-  printf("Factorial of given number is %d\n",factorial(num));
+  printf("Write number here for factorial :");
+  scanf("%lld",&num);
+  fibonacci(num);
 }
